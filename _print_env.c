55aedc534078fa3e198b8ab4f1b4658542857cab
haoningng <nick.nghn@gmail.com>
2023-12-21 02:23:22 -0800

    make betty happy;

--- _print_env.c
+++ _print_env.c
@@ -4,15 +4,15 @@ extern char **environ;
 
 /**
  * _print_env - Prints the current environment variables
- * 
+ *
  * Return: Nothing
 */
 void _print_env(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; environ[i] != NULL; i++)
-    {
-        printf("%s\n", environ[i]);
-    }
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		printf("%s\n", environ[i]);
+	}
 }
